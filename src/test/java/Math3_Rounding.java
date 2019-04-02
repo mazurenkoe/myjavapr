289cefa53e81286e62e697be7027837d5eedf590
Elena Mazurenko <mazurenko.olena@pdffiller.team>
2019-04-03 01:04:44 +0300

    Updated class Math3_Rounding

--- src/test/java/Math3_Rounding.java
+++ src/test/java/Math3_Rounding.java
@@ -4,7 +4,14 @@ public class Math3_Rounding {
 //    Создайте программу, округляющую число n до ближайшего целого и выводящую результат на экран.
 
     public static void main(String[] args) {
-        float num = 3.33f;
-        System.out.println(Math.round(num));
+
+        float num = 3.49f;
+        int intPart = (int) num;
+        float fractPart = num - (int) num;
+
+        if (fractPart >= 0.5) {
+            System.out.println(intPart + 1);
+        } else
+            System.out.println(intPart);
     }
 }
---
+++ src/test/java/lesson3/FilmInfo.java
@@ -0,0 +1,31 @@
+package lesson3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class FilmInfo {
+
+    public WebDriver driver;
+
+
+    @Test
+    public void FindFilm(){
+
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        driver = new ChromeDriver();
+        driver.get("https://www.imdb.com/chart/top");
+
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//*[contains(@class, 'lister-list')//tr/td")));
+
+        String title = (driver.findElement(By.xpath("//*[contains(@class, 'lister-list')//tr/td"))).getText();
+        System.out.println(title);
+
+        driver.quit();
+
+    }
+
+}
