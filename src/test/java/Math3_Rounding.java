6a1780930415eae9c8a2f2ed1e06c324200a58df
Elena Mazurenko <mazurenko.olena@pdffiller.team>
2019-04-02 14:12:08 +0300

    Uploaded 3 and 4 tasks

--- src/test/java/Math3_Rounding.java
+++ src/test/java/Math3_Rounding.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Math3_Rounding {
+
+//    В переменной n хранится вещественное число с ненулевой дробной частью.
+//    Создайте программу, округляющую число n до ближайшего целого и выводящую результат на экран.
+
+    public static void main(String[] args) {
+        float num = 3.33f;
+        System.out.println(Math.round(num));
+    }
 }
--- src/test/java/Math4_CountThree.java
+++ src/test/java/Math4_CountThree.java
@@ -1,2 +1,15 @@
 public class Math4_CountThree {
+
+//  В переменной n хранится натуральное трёхзначное число.
+//  Создайте программу, вычисляющую и выводящую на экран сумму цифр числа n.
+
+    public static void main(String[] args) {
+        int n = 222;
+        int result = 0;
+        String str = Integer.toString(n);
+        for(int i = 0; i < 3; i++) {
+            result += Character.getNumericValue(str.charAt(i));
+        }
+        System.out.println(result);
+    }
 }
